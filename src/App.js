a9023b7e5b8709ecf01c23781398931e60599d3c
ryo <hideyoshi.adm@gmail.com>
2024-06-20 00:43:54 +0900

    【プロジェクト名】 【概要】Material UI使ってみる 【コミット理由】

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+/* App.js */
+import * as React from "react";
+import { Button } from "@mui/material";
 
-function App() {
+const  App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Button >text</Button>
+      <Button variant="contained">contained</Button>
+      <Button variant="outlined">outlined</Button>
+    </>
   );
-}
+};
 
 export default App;
